2b9f0ab24b69d7251af4a12dc92ab6fe3d21371c
RoseCuL <59814816+RoseCuL@users.noreply.github.com>
2021-11-12 07:24:42 +0000

    Update networks.ts

--- src/config/constants/networks.ts
+++ src/config/constants/networks.ts
@@ -1,7 +1,7 @@
 import { ChainId } from '@pancakeswap/sdk'
 
 const NETWORK_URLS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: 'https://bsc-dataseed3.ninicoin.io',
+  [ChainId.MAINNET]: 'https://speedy-nodes-nyc.moralis.io/9216e41b744e85d847f78ab4/bsc/mainnet',
   [ChainId.TESTNET]: 'https://data-seed-prebsc-1-s1.binance.org:8545',
 }
 
